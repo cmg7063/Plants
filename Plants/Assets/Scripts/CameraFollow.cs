0ffc17a9c83c738b4c7d35c648c91898a207d69c
zrm2647 <zrm2647@g.rit.edu>
2018-03-01 21:08:39 -0500

    moving guy moves
    a cube moves automatically and left/right arrow keys rotate it

---
+++ Plants/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+    public Transform target;
+    public float smoothSpeed;
+    public Vector3 offset;
+    public float rotateSpeed;
+
+    // Use this for initialization
+    void Start () {
+        smoothSpeed = 0.125f;
+        rotateSpeed = 270f;
+    }
+
+    private void LateUpdate()
+    {
+        transform.position = target.position + offset;
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+            transform.Rotate(Vector3.back, -rotateSpeed * Time.deltaTime);
+
+        if (Input.GetKey(KeyCode.RightArrow))
+            transform.Rotate(Vector3.back, rotateSpeed * Time.deltaTime);
+    }
+}
---
+++ Plants/Assets/Scripts/Player.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    public float speed;
+    public float rotateSpeed;
+
+	// Use this for initialization
+	void Start () {
+        speed = 0.1f;
+        rotateSpeed = 270f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(0, speed, 0);
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+            transform.Rotate(Vector3.back, -rotateSpeed * Time.deltaTime);
+
+        if (Input.GetKey(KeyCode.RightArrow))
+            transform.Rotate(Vector3.back, rotateSpeed * Time.deltaTime);
+    }
+}
