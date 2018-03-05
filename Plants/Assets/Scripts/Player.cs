2a9364d2fc3686ef7f881a0a3962cf9b7c804d77
zrm2647 <zrm2647@g.rit.edu>
2018-03-05 11:06:03 -0500

    test for seeds and pot
    small thing

--- Plants/Assets/Scripts/Player.cs
+++ Plants/Assets/Scripts/Player.cs
@@ -6,11 +6,14 @@ public class Player : MonoBehaviour {
 
     public float speed;
     public float rotateSpeed;
+    bool wallCheck;
+    bool seedCheck;
 
 	// Use this for initialization
 	void Start () {
         speed = 0.1f;
         rotateSpeed = 270f;
+        seedCheck = false;
 	}
 	
 	// Update is called once per frame
@@ -22,5 +25,21 @@ public class Player : MonoBehaviour {
 
         if (Input.GetKey(KeyCode.RightArrow))
             transform.Rotate(Vector3.back, rotateSpeed * Time.deltaTime);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Seed")
+        {
+            Destroy(collision.gameObject);
+            Debug.Log("test");
+            seedCheck = true;
+        }
+
+        if (collision.gameObject.tag == "Pot" && seedCheck == true)
+        {
+            collision.gameObject.GetComponent<SpriteRenderer>().color = Color.blue;
+            seedCheck = false;
+        }
     }
 }
