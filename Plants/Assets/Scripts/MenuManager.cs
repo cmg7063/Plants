873dbade4b3151b2faa2242f20e380d498bad871
Corinne Green <cmg7063@g.rit.edu>
2018-03-20 15:51:41 -0400

    basic seed spawner, setup menus with basic UI, etc.

---
+++ Plants/Assets/Scripts/MenuManager.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class MenuManager : MonoBehaviour
+{
+
+    public Button instructions;
+    public Button play;
+    public Button quit;
+    public Button close;
+    public Button credits;
+    public Button menu;
+
+    private GameObject instructionPane;
+
+    private bool instructionsClicked;
+
+    // Use this for initialization
+    void Start()
+    {
+        if (SceneManager.GetActiveScene().name == "Menu")
+        {
+            instructionPane = GameObject.FindGameObjectWithTag("ShowOnClick");
+            hideText();
+        }
+    }
+
+    void hideText()
+    {
+        instructionPane.SetActive(false);
+    }
+
+    void showText()
+    {
+        instructionPane.SetActive(true);
+    }
+
+    public void Instructions()
+    {
+        showText();
+    }
+
+    public void Menu()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+
+    public void Credits()
+    {
+        SceneManager.LoadScene("Credit");
+    }
+
+    public void Close()
+    {
+        hideText();
+    }
+
+    public void Play()
+    {
+        SceneManager.LoadScene("Play");
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
---
+++ Plants/Assets/Scripts/SeedSpawner.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SeedSpawner : MonoBehaviour
+{
+
+    // Spawn this object
+    public GameObject seed;
+
+    public float maxTime = 5;
+    public float minTime = 2;
+    private float time; // current time
+
+
+    // The time to spawn the object
+    private float spawnTime;
+
+    void Start()
+    {
+        SetRandomTime();
+        time = minTime;
+    }
+
+    void FixedUpdate()
+    {
+
+        time += Time.deltaTime;
+
+        // Check if its the right time to spawn the object
+        if (time >= spawnTime)
+        {
+            SpawnSeed();
+            SetRandomTime();
+        }
+
+    }
+
+    //Spawns the object and resets the time
+    void SpawnSeed()
+    {
+        time = 0;
+        Instantiate(seed, transform.position, seed.transform.rotation);
+    }
+
+    //Sets the random time between minTime and maxTime
+    void SetRandomTime()
+    {
+        spawnTime = Random.Range(minTime, maxTime);
+    }
+
+}
