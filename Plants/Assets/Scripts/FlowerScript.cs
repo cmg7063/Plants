70b9f09fd046576d17b2e5facb75dc6f094248a8
SungminPark1 <sp4216@rit.edu>
2018-03-29 20:10:31 -0400

    added logic for player collision with flower (#3)

---
+++ Plants/Assets/Scripts/FlowerScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlowerScript : MonoBehaviour {
+	private bool hurtPlayer;
+
+	// Use this for initialization
+	void Start () {
+		hurtPlayer = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public bool getHurtPlayer() {
+		return hurtPlayer;
+	}
+
+	public void toggleHurtPlayer() {
+		hurtPlayer = !hurtPlayer;
+	}
+}
